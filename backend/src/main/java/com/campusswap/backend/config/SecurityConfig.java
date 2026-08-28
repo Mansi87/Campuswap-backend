fb4a17fdeaae2cb055f9718f4c9524eb1047cd47
Mansi87 <manupandit053@gmail.com>
2026-08-28 11:16:44 +0530

    Fixed backend cors issue

--- backend/src/main/java/com/campusswap/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/campusswap/backend/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*")); // React app
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://campusswap-frontend.vercel.app")); // React app
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
